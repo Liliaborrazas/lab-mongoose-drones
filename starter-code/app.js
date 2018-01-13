1af752faaa79e1a1c910a3b91d1dc0ace86de206
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2018-01-13 18:32:40 +0100

    modified app.js

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,14 +1,17 @@
-const express        = require('express');
-const path           = require('path');
-const favicon        = require('serve-favicon');
-const logger         = require('morgan');
-const cookieParser   = require('cookie-parser');
-const bodyParser     = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
-const mongoose       = require('mongoose');
-const index          = require('./routes/index');
-const users          = require('./routes/users');
-const drones         = require('./routes/drones');
+const mongoose = require('mongoose');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const drones = require('./routes/drones');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const drones = require('./routes/drones');
 
 mongoose.connect('mongodb://localhost/drones-dev');
 
@@ -25,12 +28,15 @@ app.locals.title = 'Drnz';
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/drones', drones);
+app.use('/', drones);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
